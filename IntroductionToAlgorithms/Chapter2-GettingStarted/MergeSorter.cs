842d6a005e6b46a2ad53f1c543f4a8e6ed258eaf
Ram Kumar Hariharan <coolramkumar@gmail.com>
2014-09-14 10:01:06 -0700

    Merge sort + unit tests.

---
+++ IntroductionToAlgorithms/Chapter2-GettingStarted/MergeSorter.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace com.techsnippets.IntroductionToAlgorithms.Chapter2GettingStarted
+{
+    /// <summary>
+    /// Class that perform merge sort.
+    /// </summary>
+    internal sealed class MergeSorter : ISorter
+    {
+        /// <summary>
+        /// Performs merge sort on the given list.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the list.</typeparam>
+        /// <param name="input">The input list</param>
+        public void Sort<T>(List<T> input) where T : IComparable<T>
+        {
+            if (input == null)
+            {
+                return;
+            }
+
+            MergeSort(input, 0, input.Count - 1);
+        }
+
+        /// <summary>
+        /// Performs merge sort on list between and including the given indices.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the list.</typeparam>
+        /// <param name="input">The input list.</param>
+        /// <param name="p">The begin index.</param>
+        /// <param name="r">The end index.</param>
+        private void MergeSort<T>(List<T> input, int p, int r) where T: IComparable<T>
+        {
+            if (p < r)
+            {
+                int q = (p + r) / 2;
+                MergeSort(input, p, q);
+                MergeSort(input, q + 1, r);
+                Merge(input, p, q, r);
+            }
+        }
+
+        /// <summary>
+        /// Merges a list that is sorted separately into a single sorted list.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the list.</typeparam>
+        /// <param name="input">The input list.</param>
+        /// <param name="p">The begin index if sorted first half.</param>
+        /// <param name="q">The end index of sorted first half.</param>
+        /// <param name="r">The end index of sorted second half.</param>
+        /// <remarks>
+        /// The begin index of sorted second half is <paramref name="q"/> + 1.
+        /// </remarks>
+        private void Merge<T>(List<T> input, int p, int q, int r) where T: IComparable<T>
+        {
+            var n1 = q - p + 1;
+            var n2 = r - q;
+            var left = new List<T>(n1);
+            var right = new List<T>(n2);
+
+            for (var i = 0; i < n1; i++) left.Add(input[p + i]);
+            for (var j = 0; j < n2; j++) right.Add(input[q + j + 1]);
+
+            var x = 0;
+            var y = 0;
+            for (var k = p; k <= r; k++)
+            {
+                if (y >= n2 || (x < n1 && left[x].CompareTo(right[y]) <= 0))
+                {
+                    input[k] = left[x];
+                    x++;
+                }
+                else
+                {
+                    input[k] = right[y];
+                    y++;
+                }
+            }
+        }
+    }
+}
---
+++ IntroductionToAlgorithms/Chapter2GettingStarted.UnitTests/MergeSorterTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace com.techsnippets.IntroductionToAlgorithms.Chapter2GettingStarted.UnitTests
+{
+    /// <summary>
+    /// Tests the <see cref="MergeSorter"/> class.
+    /// </summary>
+    [TestClass]
+    public class MergeSorterTests
+    {
+        /// <summary>
+        /// Tests the <see cref="MergeSorter.Sort{T}"/> method
+        /// when an null argument is passed.
+        /// </summary>
+        [TestMethod]
+        public void TestNullList()
+        {
+            var sorter = new MergeSorter();
+            sorter.Sort<int>(null);
+        }
+
+        /// <summary>
+        /// Tests the <see cref="MergeSorter.Sort{T}"/> method
+        /// when an empty list is passed.
+        /// </summary>
+        [TestMethod]
+        public void TestEmptyList()
+        {
+            var sorter = new MergeSorter();
+            var list = new List<int> { };
+            sorter.Sort(list);
+            Assert.AreEqual(0, list.Count);
+        }
+
+        /// <summary>
+        /// Tests the <see cref="MergeSorter.Sort{T}"/> method
+        /// when an proper list is passed.
+        /// </summary>
+        [TestMethod]
+        public void TestProperList()
+        {
+            var sorter = new MergeSorter();
+            var list = new List<int> { 3, 2, 1, 9, 7 };
+            sorter.Sort(list);
+            Assert.AreEqual(5, list.Count);
+            Assert.IsTrue(list.SequenceEqual(new List<int> { 1, 2, 3, 7, 9 }));
+        }
+    }
+}
